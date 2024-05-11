84de3457b852c5a100918e6021cd7d8dda766f85
sakib6619 <sahadothosain6619@gmail.com>
2024-05-12 00:23:24 +0600

    added story book complite

--- storyBook.py
+++ storyBook.py
@@ -1,4 +1,12 @@
 class StoryBook:
-    pass
+    def __init__(self,name,author,price,authorBorn):
+        self.name = name
+        self.author = author
+        self.price = price
+        self.authorBorn = authorBorn
+
+
+book1 = StoryBook('programing','sakib',300,'25-12-2003')
+print(book1.name)
 
 
